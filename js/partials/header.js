f2a4a3db8ff2bf07ed4dd00be78927cf7af5cecc
Oscar Dahlquist <43441099+Vattenkruka@users.noreply.github.com>
2020-11-04 13:08:36 +0100

    setup complete

---
+++ js/partials/header.js
@@ -0,0 +1,6 @@
+document.getElementById("header").innerHTML = `<header>
+<nav class="navbar navbar-teal bg-dark">
+        <a class="navbar-brand" href="index.html">Artist Showcase</a>
+        </div>
+      </nav>
+      </header>`
