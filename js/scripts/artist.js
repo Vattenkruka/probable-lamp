2416495c88309e6bfba8dbd893fe18100337771b
Oscar Dahlquist <43441099+Vattenkruka@users.noreply.github.com>
2020-11-04 16:39:14 +0100

    Vattenkruka fix: refactoring

--- js/scripts/artist.js
+++ js/scripts/artist.js
@@ -1,17 +1,17 @@
 
 /*Declaration of artists and list */
 export const artists = [];
-const eltonJohn = new Artist("Elton John", "United Kingdom", "Rock","public/images/eltonJohn.jpg");
-const eminem = new Artist("Eminem", "USA", "Rap","public/images/eminem.jpg");
-const adele = new Artist("Adele", "United Kingdom", "Soul","public/images/adele.jpg");
-const coldplay = new Artist("Coldplay", "United Kingdom", "Alternative Rock","public/images/coldplay.jpg");
-const gorillaz = new Artist("Gorillaz", "United Kingdom", "Alternative Rock","public/images/gorillaz.jpg");
+const eltonJohn = new Artist("Elton John", "United Kingdom", "Rock", "public/images/eltonJohn.jpg");
+const eminem = new Artist("Eminem", "USA", "Rap", "public/images/eminem.jpg");
+const adele = new Artist("Adele", "United Kingdom", "Soul", "public/images/adele.jpg");
+const coldplay = new Artist("Coldplay", "United Kingdom", "Alternative Rock", "public/images/coldplay.jpg");
+const gorillaz = new Artist("Gorillaz", "United Kingdom", "Alternative Rock", "public/images/gorillaz.jpg");
 
 const lyrics = new Map([
-    [eltonJohn.name ,'And it seems to me you lived your life like a candle in the wind'],
+    [eltonJohn.name, 'And it seems to me you lived your life like a candle in the wind'],
     [eminem.name, 'His palms are sweaty, knees weak, arms are heavy'],
     [adele.name, 'There\'s a fire starting in my heart'],
-    [coldplay.name,'I used to rule the world'],
+    [coldplay.name, 'I used to rule the world'],
     [gorillaz.name, 'Sha, sha ba da, sha ba da, feel good']
 
 ])
